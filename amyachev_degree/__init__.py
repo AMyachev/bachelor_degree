9697001371067702093562bd6dd69caf284806b4
Anatolii Myachev <anatoliimyachev@mail.ru>
2019-05-11 12:50:23 +0300

    renamed 'io_my.py' -> 'io.my'

--- amyachev_degree/__init__.py
+++ amyachev_degree/__init__.py
@@ -1,6 +1,6 @@
 from amyachev_degree.core import (  # noqa
     create_schedule, flow_job_generator, johnson_three_machines_generator)
-from amyachev_degree.io_my import read_flow_shop_instances  # noqa
+from amyachev_degree.io import read_flow_shop_instances  # noqa
 from amyachev_degree.simple_heuristics import (  # noqa
     neh_heuristics, palmer_heuristics, cds_heuristics)
 from amyachev_degree.exact_algorithm import johnson_algorithm  # noqa
