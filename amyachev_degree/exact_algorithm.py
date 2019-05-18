370a182e7e8541cf7d74ff76eef92e8b72f50708
Anatolii Myachev <anatoliimyachev@mail.ru>
2019-05-18 17:12:50 +0300

    some changes for 'johnson_algorithm'

--- amyachev_degree/exact_algorithm.py
+++ amyachev_degree/exact_algorithm.py
@@ -22,25 +22,31 @@ def johnson_algorithm(frame: JobSchedulingFrame) -> list:
     # init job indexes
     exact_solution = [i for i in range(frame.count_jobs)]
 
+    # sorting by increasing the minimum processing time
     exact_solution.sort(
-        key=lambda job_index: min(
-            frame.get_processing_time(job_index, 0),
-            frame.get_processing_time(job_index, 1)
+        key=lambda idx_job: min(
+            frame.get_processing_time(idx_job, idx_machine=0),
+            frame.get_processing_time(idx_job, idx_machine=1)
         )
     )
 
-    first_machine_jobs = []
-    second_machine_jobs = []
-    for job_index in exact_solution:
-        frst = frame.get_processing_time(job_index, 0)
-        scnd = frame.get_processing_time(job_index, 1)
-        if frst < scnd:
-            first_machine_jobs.append(job_index)
-        else:
-            second_machine_jobs.append(job_index)
+    jobs_on_first_machine = []
+    jobs_on_second_machine = []
 
-    exact_solution = first_machine_jobs
-    second_machine_jobs.reverse()
-    exact_solution.extend(second_machine_jobs)
+    # distribution of jobs on machines
+    for idx_job in exact_solution:
+        frst = frame.get_processing_time(idx_job, idx_machine=0)
+        scnd = frame.get_processing_time(idx_job, idx_machine=1)
+        if frst < scnd:
+            jobs_on_first_machine.append(idx_job)
+        else:
+            jobs_on_second_machine.append(idx_job)
+
+    exact_solution = jobs_on_first_machine
+
+    # simulating the installation of jobs that are processed faster on the
+    # second machine to the end of the processing queue on the first machine
+    jobs_on_second_machine.reverse()
+    exact_solution.extend(jobs_on_second_machine)
 
     return exact_solution
