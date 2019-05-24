57c5c71da2e21e2450fc59af0facb1ede2bbd5f3
Anatolii Myachev <anatoliimyachev@mail.ru>
2019-05-24 14:26:18 +0300

    added 'my_round' func

--- amyachev_degree/util/testing.py
+++ amyachev_degree/util/testing.py
@@ -3,3 +3,22 @@ from amyachev_degree.core import JobSchedulingFrame
 
 def assert_js_frame(fst: JobSchedulingFrame, scnd: JobSchedulingFrame) -> bool:
     assert str(fst) == str(scnd)
+
+
+def my_round(value):
+    """
+    Arithmetic rounding.
+
+    Parameters
+    ----------
+    value: numeric value
+
+    Returns
+    -------
+    : int
+
+    Notes
+    -----
+    Python3 uses default banking rounding.
+    """
+    return int(value + 0.5)
