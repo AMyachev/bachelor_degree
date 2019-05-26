d0693fe26330dd1b0a6b7aab021698a2c8e4bcf8
Anatolii Myachev <anatoliimyachev@mail.ru>
2019-05-26 21:31:51 +0300

    added 'test_neh_heuristic_three_machines', 'test_liu_reeves_heuristic_three_machines'

--- amyachev_degree/tests/test_johnson_problem_algorithms.py
+++ amyachev_degree/tests/test_johnson_problem_algorithms.py
@@ -198,3 +198,83 @@ class TestJohnsonProblems:
             palmer_heuristics, {}, cds_heuristics, {}, frames)
 
         assert round(average_percent_ratio, 2) == expected_percent_ratio
+
+    @pytest.mark.parametrize('count_jobs, time_seed, expected_percent_ratio',
+                             [(20, 873654221, 0.00),
+                              (50, 379008056, 0.00),
+                              # too long time for regular testing
+                              # (100, 1866992158, 0.00),
+                              # (200, 216771124, 0.00),
+                              # (500, 495070989, 0.00)
+                              ])
+    def test_neh_heuristic_three_machines(self, count_jobs, time_seed,
+                                          expected_percent_ratio):
+        """
+        Function for research.
+
+        Problem
+        -------
+        Johnson's problem of three machines.
+
+        Abstract
+        --------
+        The experiment consists in comparing the results of NEH heuristic
+        with the exact solution found by CDS algorithm(sub_problem=2).
+
+        Notes
+        -----
+        Starts as follows (from root folder):
+            `pytest amyachev_degree/tests/test_johnson_problem_algorithms.py\
+             ::TestJohnsonProblems::test_neh_heuristic_three_machines`
+
+        All tests run about 860 sec.
+
+        """
+        frames = self.generation_problems(
+            count_problem=10, generator=johnson_three_machines_generator,
+            count_jobs=count_jobs, initial_seed=time_seed)
+
+        average_percent_ratio = percentage_deviation(
+            neh_heuristics, {}, cds_heuristics, {}, frames)
+
+        assert round(average_percent_ratio, 2) == expected_percent_ratio
+
+    @pytest.mark.parametrize('count_jobs, time_seed, expected_percent_ratio',
+                             [(20, 873654221, 1.4),
+                              (50, 379008056, 0.53),
+                              (100, 1866992158, 0.26),
+                              (200, 216771124, 0.13),
+                              (500, 495070989, 0.05)
+                              ])
+    def test_liu_reeves_heuristic_three_machines(self, count_jobs, time_seed,
+                                                 expected_percent_ratio):
+        """
+        Function for research.
+
+        Problem
+        -------
+        Johnson's problem of three machines.
+
+        Abstract
+        --------
+        The experiment consists in comparing the results of LR(5) heuristic
+        with the exact solution found by CDS algorithm(sub_problem=2).
+
+        Notes
+        -----
+        Starts as follows (from root folder):
+            `pytest amyachev_degree/tests/test_johnson_problem_algorithms.py\
+             ::TestJohnsonProblems::test_liu_reeves_heuristic_three_machines`
+
+        All tests run about 1.52 sec.
+
+        """
+        frames = self.generation_problems(
+            count_problem=10, generator=johnson_three_machines_generator,
+            count_jobs=count_jobs, initial_seed=time_seed)
+
+        average_percent_ratio = percentage_deviation(
+            liu_reeves_heuristics, {'count_sequences': 5},
+            cds_heuristics, {}, frames)
+
+        assert round(average_percent_ratio, 2) == expected_percent_ratio
