5917df79304820accc9d274c7b2fa5491b5d5055
Anatolii Myachev <anatoliimyachev@mail.ru>
2019-05-27 14:57:54 +0300

    added 'percentage_deviation_using_upper_bound' func

--- amyachev_degree/util/testing.py
+++ amyachev_degree/util/testing.py
@@ -65,3 +65,39 @@ def percentage_deviation(fst_heuristic: object, fst_args: dict,
         solutions_ratio += end_time_diff / scnd_end_time
 
     return solutions_ratio / len(frames) * 100
+
+
+def percentage_deviation_using_upper_bound(fst_heuristic: object,
+                                           fst_args: dict,
+                                           frames: list) -> float:
+    """
+    The calculations are performed with respect to the results that are
+    stored by frames in `upper_bound` property.
+
+    Parameters
+    ----------
+    fst_heuristic: object
+        function callback
+    fst_args: dict
+        named arguments for `fst_heuristic`
+    frames: list
+        list of `JobSchedulingFrame` objects
+
+    Returns
+    -------
+    average_deviation: float
+
+    Notes
+    -----
+    Averaging occurs by the count of frames.
+
+    """
+    solutions_ratio = 0.
+    for frame in frames:
+        fst_solution = fst_heuristic(frame, **fst_args)
+        fst_end_time = compute_end_time(frame, fst_solution)
+
+        end_time_diff = fst_end_time - frame.upper_bound
+        solutions_ratio += end_time_diff / frame.upper_bound
+
+    return solutions_ratio / len(frames) * 100
